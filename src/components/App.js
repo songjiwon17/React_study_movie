3201e25ed6d61df229b98da844e99d72dca9e7fd
song <tmddnjs0633@gmail.com>
2023-09-28 13:38:38 +0900

    App-six-commit

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,7 @@ function App(){
         if(options.offset === 0){
             setItems(reviews);
         }else{
-            setItems([...items, ...reviews]);
+            setItems((prevItems)=>[...prevItems, ...reviews]);
         }
         setOffset(options.offset + reviews.length);
         setHasNext(paging.hasNext);
