9682dbf36a0b76f7fba487e7b08617e78186c23b
song <tmddnjs0633@gmail.com>
2023-10-08 19:54:37 +0900

    ReviewForm-oneState-commit

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -2,36 +2,33 @@ import { useState } from "react";
 import './ReviewForm.css';
 
 function ReviewForm(){
-    const [title, setTitle] = useState('');
-    const [rating, setRating] = useState(0);
-    const [content, setContent] = useState('');
 
-    const handleTitleChange = (e)=>{
-        setTitle(e.target.value);
-    }
+    const [values, setValues] = useState({
+        title:'',
+        rating: 0,
+        content:''
+    });
 
-    const handleRatingChange = (e)=>{
-        const nextRating = Number(e.target.value) || 0;
-        setRating(nextRating);
-    }
-
-    const handleContentChange = (e)=>{
-        setContent(e.target.value);
+    const handleChange = (e)=>{
+        const {name, value } = e.target;
+        setValues((prevValues)=>({
+            ...prevValues,
+            [name]: value
+        }))
     }
 
     const handleSubmit = (e)=>{
         e.preventDefault();
-        console.log({title, rating, content})
+        console.log(values)
     }
 
     return(
         <form className="ReviewForm" onSubmit={handleSubmit}>
-            <input value={title} onChange={handleTitleChange}></input>
-            <input type="number" value={rating} onChange={handleRatingChange}></input>
-            <textarea value={content} onChange={handleContentChange}></textarea>
+            <input name='title' value={values.title} onChange={handleChange}></input>
+            <input type="number" name="rating" value={values.rating} onChange={handleChange}></input>
+            <textarea name="content" value={values.content} onChange={handleChange}></textarea>
             <button type="submit">확인</button>
-        </form>
+        </form> 
     )
 }
-
 export default ReviewForm;
