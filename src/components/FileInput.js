97304cfbda70c5c4636b97ced7178106b717ffe2
song <tmddnjs0633@gmail.com>
2023-10-11 13:07:46 +0900

    FileInput-commit

---
+++ src/components/FileInput.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function FileInput(){
+    const [value, setValues] = useState();
+
+    const handleChange = (e)=>{
+        console.log(e.target.files);
+    }
+
+    return <input type="file" onChange={handleChange}/>
+}
+export default FileInput;
