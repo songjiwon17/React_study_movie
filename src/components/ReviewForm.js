ea77fa6bf41cf0da07b84307263ee9218d2af6f0
song <tmddnjs0633@gmail.com>
2023-10-11 20:08:14 +0900

    ReviewForm-FileInput2-commit

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -7,15 +7,20 @@ function ReviewForm(){
     const [values, setValues] = useState({
         title:'',
         rating: 0,
-        content:''
+        content:'',
+        imgFile: null
     });
 
-    const handleChange = (e)=>{
-        const {name, value } = e.target;
+    const handleChange = (name, value)=>{
         setValues((prevValues)=>({
             ...prevValues,
             [name]: value
         }))
+    }
+
+    const handleInputChange = (e)=>{
+        const {name, value} = e.target;
+        handleChange(name, value);
     }
 
     const handleSubmit = (e)=>{
@@ -25,10 +30,10 @@ function ReviewForm(){
 
     return(
         <form className="ReviewForm" onSubmit={handleSubmit}>
-            <FileInput />
-            <input name='title' value={values.title} onChange={handleChange}></input>
-            <input type="number" name="rating" value={values.rating} onChange={handleChange}></input>
-            <textarea name="content" value={values.content} onChange={handleChange}></textarea>
+            <FileInput name={"imgFile"} value={values.imgFile} onChange={handleChange}/>
+            <input name='title' value={values.title} onChange={handleInputChange}></input>
+            <input type="number" name="rating" value={values.rating} onChange={handleInputChange}></input>
+            <textarea name="content" value={values.content} onChange={handleInputChange}></textarea>
             <button type="submit">확인</button>
         </form> 
     )
