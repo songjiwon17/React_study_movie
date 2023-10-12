578524eae5f0165e9a59dfe5ad5e592e640abcbd
song <tmddnjs0633@gmail.com>
2023-10-12 13:03:52 +0900

    FileInput-handleClearClick-commit

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,11 +1,26 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function FileInput({name, value, onChange}){
+    const inputRef = useRef();
+
     const handleChange = (e)=>{
         const nextValue = e.target.files;
         onChange(name, nextValue);
     }
 
-    return <input type="file" onChange={handleChange}/>
+    const handleClearClick = ()=>{
+        const inputNode = inputRef.current;
+        if(!inputNode) return;
+
+        inputNode.value = '';
+        onChange(name, null);
+    }
+
+    return(
+        <div>
+            <input type="file" onChange={handleChange} ref={inputRef}/>
+            {value && <button onClick={handleClearClick}>초기화</button>}
+        </div>
+    ) 
 }
 export default FileInput;
