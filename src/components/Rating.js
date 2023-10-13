f5fc736ef764b07a2138416b9a6ba81cfaf413c8
song <tmddnjs0633@gmail.com>
2023-10-13 23:28:00 +0900

    Rating-commit

---
+++ src/components/Rating.js
@@ -0,0 +1,17 @@
+function Star({selected = false}) {
+    const className = `Rating-star ${selected ? 'selected' : ''}`;
+    return <span className={className}>❤</span>
+}
+
+function Rating({value = 0}) {
+    return(
+        <div>
+            <Star selected={value>=1}/>
+            <Star selected={value>=2}/>
+            <Star selected={value>=3}/>
+            <Star selected={value>=4}/>
+            <Star selected={value>=5}/>
+        </div>
+    )
+}
+export default Rating;
