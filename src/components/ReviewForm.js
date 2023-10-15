fdd81ff4ad5722fd5925bd9045337720a0468570
song <tmddnjs0633@gmail.com>
2023-10-15 16:39:04 +0900

    RatingInput 컴포넌트 추가하고 onChange handleChange로 바꾸기

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import './ReviewForm.css';
 import FileInput from "./FileInput";
+import RatingInput from "./RatingInput";
 
 function ReviewForm(){
 
@@ -32,7 +33,7 @@ function ReviewForm(){
         <form className="ReviewForm" onSubmit={handleSubmit}>
             <FileInput name={"imgFile"} value={values.imgFile} onChange={handleChange}/>
             <input name='title' value={values.title} onChange={handleInputChange}></input>
-            <input type="number" name="rating" value={values.rating} onChange={handleInputChange}></input>
+            <RatingInput name="rating" value={values.rating} onChange={handleChange}/>
             <textarea name="content" value={values.content} onChange={handleInputChange}></textarea>
             <button type="submit">확인</button>
         </form> 
