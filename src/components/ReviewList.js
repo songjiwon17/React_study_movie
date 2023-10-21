69ebc0ca97db81c6ca03eb388057739f16e6685c
song <tmddnjs0633@gmail.com>
2023-10-21 10:58:06 +0900

    수정버튼 추가_수정버튼 클릭 시 입력폼

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import Rating from './Rating';
 import './ReviewList.css';
+import ReviewForm from './ReviewForm';
 
 function formatDate(value){
     const date = new Date(value);
     return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`
 }
 
-function ReviewListItem({item, onDelete}){ //
+function ReviewListItem({item, onDelete, onEdit}){
     const handleDeleteClick = ()=>onDelete(item.id);
+
+    const handleEditClick = ()=>{
+        onEdit(item.id);
+    }
+
     return(
         <div className="ReviewListItem">
             <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title}/>
@@ -17,18 +24,28 @@ function ReviewListItem({item, onDelete}){ //
                 <p>{formatDate(item.createdAt)}</p>
                 <p>{item.content}</p>
                 <button onClick={handleDeleteClick}>삭제</button>
+                <button onClick={handleEditClick}>수정</button>
             </div>
         </div>
     )
 }
 
 function ReviewList({items, onDelete}){
+    const [editingId, setEditingId] = useState(null);
+
     return (
         <ul>
         {items.map((item)=>{
+            if(item.id === editingId){
+                return (
+                    <li key={item.id}>
+                        <ReviewForm />
+                    </li>
+                );
+            }
         return (
             <li key={item.id}>
-                <ReviewListItem item={item} onDelete={onDelete}/>
+                <ReviewListItem item={item} onDelete={onDelete} onEdit={setEditingId}/>
             </li>
         );
         })}
