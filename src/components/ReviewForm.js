d58065336f48b008e1f00f2ca8b183ade3c6e1d3
song <tmddnjs0633@gmail.com>
2023-10-25 19:28:35 +0900

    취소버튼 추가_onCancle

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -11,11 +11,11 @@ const INITIAL_VALUES = {
     imgFile: null
 }
 
-function ReviewForm({ onSubmitSuccess }){
+function ReviewForm({ initialValues = INITIAL_VALUES, initialPreview, onSubmitSuccess, onCancel}){
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [submittingError, setSubmittingError] = useState(null);
 
-    const [values, setValues] = useState(INITIAL_VALUES);
+    const [values, setValues] = useState(initialValues);
 
     const handleChange = (name, value)=>{
         setValues((prevValues)=>({
@@ -58,10 +58,11 @@ function ReviewForm({ onSubmitSuccess }){
 
     return(
         <form className="ReviewForm" onSubmit={handleSubmit}>
-            <FileInput name={"imgFile"} value={values.imgFile} onChange={handleChange}/>
+            <FileInput name={"imgFile"} value={values.imgFile} initialPreview={initialPreview} onChange={handleChange}/>
             <input name='title' value={values.title} onChange={handleInputChange}></input>
             <RatingInput name="rating" value={values.rating} onChange={handleChange}/>
             <textarea name="content" value={values.content} onChange={handleInputChange}></textarea>
+            {onCancel && <button onClick={onCancel}>취소</button>}
             <button type="submit" disabled={isSubmitting}>확인</button>
             {submittingError?.message && <div>{submittingError.message}</div>}
         </form> 
