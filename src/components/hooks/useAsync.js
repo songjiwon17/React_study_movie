5fd42dd64469dea9628b3e029f4246ae497bf5b3
song <tmddnjs0633@gmail.com>
2023-10-27 10:49:09 +0900

    비동기 useAsync함수 생성

---
+++ src/components/hooks/useAsync.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function useAsync (asyncFunction){
+    const [pending, setPending] = useState(false);
+    const [error, setError] = useState(null);
+
+    const wrappedFunction = async (...args)=>{
+        try {
+            setError(null);
+            setPending(true);
+            return await asyncFunction(...args); //스프레드 문법 사용(wrappedFunction에서 받은 파라미터를 그대로 asynFunction에다가 전달해줄 수 있다. )
+        }catch (error){
+            setError(error);
+            return;
+        }finally{
+            setPending(false);
+        }
+    }
+
+    return [pending, error, wrappedFunction];
+}
+export default useAsync;
